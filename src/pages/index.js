a194951f653705b0d08b05d4fe70612b1e902d58
nishant219 <nishantap.a21@gmail.com>
2023-06-19 14:38:54 +0530

    Update my code

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,14 +56,15 @@ function Home(){
                   className=" cursor-pointer text-2xl "
                 />
               </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
-              </li>
+<li>
+  <a
+    className="bg-gradient-to-r from-cyan-500 text-to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+    href="E:\Portfolio 2023\April2023\portfolio2023\src\pages\NishantResumeMay2023.pdf"  // Replace with the actual path to your resume file
+    download
+  >
+    Resume
+  </a>
+</li>
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
